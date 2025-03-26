b980e5da520ecc92aebe0e6dcdd3ca401d6f285b
Vass-Inc <sarumankeeb@gmail.com>
2025-03-26 18:19:32 +0000

    first commit

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+use clap::Parser;
+use rand::prelude::*;
+use std::fs::OpenOptions;
+use std::io::{self, Write};
+
+fn generate_password(size: u32) -> String {
+
+    // TODO: 
+    // - mudar a str de chars para uma autogerada 
+    // - size minimo aceite tem de ser 12
+    // - ... 
+
+
+    let todas = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_=!#$%&()*+,-.:'/?@";
+
+    let mut rng = rand::rng();
+    let mut password  = String::new();
+
+    for _ in 0..size {
+        password.push(todas.chars().choose(&mut rng).unwrap());
+    }
+ 
+    return password;
+}
+
+// TODO: 
+// decidir guardar em ficheiro cifrado ou password é guardada cifrada mesmo sendo aleatoria????
+fn save_pass(file: &str, name: &str, password: &str) -> io::Result<()>{
+    let mut file = OpenOptions::new().create(true).append(true).open(file)?;
+
+    writeln!(file, "{}: {}", name, password)?;
+
+    Ok(())
+}
+
+// FIXME: 
+// Organizar código!!
+
+/// Simple program
+#[derive(Parser, Debug)]
+#[command(name = "Minipass")]
+#[command(about = "Passwords geradas aleotoriamente e guardadas num ficheiro", long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    name: String,
+
+    #[arg(short, long, default_value_t = 1)]
+    size: u32,
+
+    #[arg(short, long, default_value = "passwords.txt")]
+    file: String,
+}
+
+
+fn main() {
+    //let pass = generate_password(12);
+    //println!("Password: {}", pass);
+
+    let args = Args::parse();
+
+    let password = generate_password(args.size);
+    println!("Password => {}", password);
+
+    if let Err(e) = save_pass(&args.file, &args.name, &password) {
+        eprintln!("erro ao guardar: {}", e);
+    } else {
+        println!("Password guardada com sucesso em {}", args.file);
+    }
+}
+
+
---
+++ todo.md
@@ -0,0 +1,6 @@
+# Tarefas para este minipass
+
+[] - minipass --help ?? 
+[] - cifrar o ficheiro onde as passwords são guardadas
+[] - abrir o ficheiro atraves da app like (minipass --show "gmail") e mostra tal pass
+[] - to be continued
